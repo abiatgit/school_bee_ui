f488c1e0c2cafebe99d6dfa1f46d5897da4c58e6
Abi George <contactabisgeorge@gmail.com>
2025-03-14 19:06:23 +0000

    Protect routes based on authentication status

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,25 @@
+import { metadata } from './app/layout';
 import { clerkMiddleware , createRouteMatcher} from "@clerk/nextjs/server";
+import { routeAccessMap} from "@/lib/settings"
+import { NextResponse } from 'next/server';
 
-const isProtectedRoute = createRouteMatcher(['/admin(.*)', '/teacher(.*)'])
+const matchers=Object.keys( routeAccessMap).map(route=>({
+  matcher:createRouteMatcher([route]),
+  allowedRoles:routeAccessMap[route]
+}))
 
-export default clerkMiddleware();
+
+
+export default clerkMiddleware(async (auth, req) => {
+ const {sessionClaims}=await auth();
+ const role=(sessionClaims?.metadata as {role?:string})?.role;
+ for(const {matcher,allowedRoles} of matchers){
+    if(matcher(req) && !allowedRoles.includes(role!)){
+      return NextResponse.redirect(new URL (`/${role}`,req.url))
+    }
+ }
+
+})
 
 export const config = {
   matcher: [
