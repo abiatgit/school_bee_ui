289dab43f846fad9874c14cdb7ff197f542295ae
Abi George <contactabisgeorge@gmail.com>
2025-03-25 11:15:10 +0000

    Feat:announcementDataFetch

--- src/components/AnnouncementsContainer.tsx
+++ src/components/AnnouncementsContainer.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Announcements from './Announcements'
 
 const AnnouncementsContainer = () => {
   return (
-    <div>
-      
+    <div className="bg-white p-4 rounded-md">
+    <div className="flex justify-between ">
+      <h1 className="text-2xl font-semibold">Announcements</h1>
+      <span className="text-xs text-gray-500">View all</span>
+    </div>
+    <div className="flex flex-col gap-4 mt-4">
+        <Announcements/>
+
+      </div>
     </div>
   )
 }
